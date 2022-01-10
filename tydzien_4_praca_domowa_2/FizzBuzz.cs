7f4679208e2f2af294aa9fd4a5976de9d9d935f0
Hubert Bąk <hobo-11@o2.pl>
2022-01-10 22:24:28 +0100

    Dodaj pliki projektów.

---
+++ tydzien_4_praca_domowa_2/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tydzien_4_praca_domowa_2
+{
+    class FizzBuzz
+    {
+        public string CheckNumber(int numberToTest)
+        {
+            if (numberToTest % 3 == 0 && numberToTest % 5 == 0) 
+            {
+                return "FizzBuzz";
+            }
+            else if (numberToTest % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (numberToTest % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return numberToTest.ToString();
+            }
+        }
+    }
+}
---
+++ tydzien_4_praca_domowa_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tydzien_4_praca_domowa_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę całkowitą, lub zakończ wprowadzając 't'");
+                var fizzBuzz = new FizzBuzz();
+                var resultToDisplay = fizzBuzz.CheckNumber(GetUserIntInput());
+                Console.WriteLine(resultToDisplay);
+            }
+        }
+
+        private static int GetUserIntInput()
+        {
+            while (true)
+            {
+                var shouldExit = Console.ReadLine();
+                if(shouldExit.ToUpper() == "T")
+                {
+                    Environment.Exit(0);
+                }
+                if(!int.TryParse(shouldExit, out int result)){
+                    Console.WriteLine("Błędne wprowadzenie, wprowadź liczbę całkowitą!");
+                    continue;
+                }
+                return result;
+            }
+        }
+    }
+}
